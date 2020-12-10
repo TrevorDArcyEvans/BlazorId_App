c5471ac7228e20efcd74aedcc3fcc392aa49a957
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2020-12-10 09:26:04 +0000

    - remove IIS config as not available on Linux

--- IdentityServerAspNetIdentity/Startup.cs
+++ IdentityServerAspNetIdentity/Startup.cs
@@ -29,51 +29,27 @@ namespace IdentityServerAspNetIdentity
         {
             services.AddControllersWithViews();
 
-            // configures IIS out-of-proc settings (see https://github.com/aspnet/AspNetCore/issues/14882)
-            services.Configure<IISOptions>(iis =>
-            {
-                iis.AuthenticationDisplayName = "Windows";
-                iis.AutomaticAuthentication = false;
-            });
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
-            // configures IIS in-proc settings
-            services.Configure<IISServerOptions>(iis =>
-            {
-                iis.AuthenticationDisplayName = "Windows";
-                iis.AutomaticAuthentication = false;
-            });
+			services.AddIdentity<ApplicationUser, IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddDefaultTokenProviders();
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+			var builder = services.AddIdentityServer(options =>
+	            {
+		            options.Events.RaiseErrorEvents = true;
+		            options.Events.RaiseInformationEvents = true;
+		            options.Events.RaiseFailureEvents = true;
+		            options.Events.RaiseSuccessEvents = true;
+	            })
+	            .AddInMemoryIdentityResources(Config.Ids)
+	            .AddInMemoryApiResources(Config.Apis)
+	            .AddInMemoryClients(Config.Clients)
+				.AddAspNetIdentity<ApplicationUser>();
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-            
-            var builder = services.AddIdentityServer(options =>
-                {
-                    options.Events.RaiseErrorEvents = true;
-                    options.Events.RaiseInformationEvents = true;
-                    options.Events.RaiseFailureEvents = true;
-                    options.Events.RaiseSuccessEvents = true;
-                })
-                .AddInMemoryIdentityResources(Config.Ids)
-                .AddInMemoryApiResources(Config.Apis)
-                .AddInMemoryClients(Config.Clients)
-                .AddAspNetIdentity<ApplicationUser>();
-
-            // not recommended for production - you need to store your key material somewhere secure
-            builder.AddDeveloperSigningCredential();
-
-            //services.AddAuthentication()
-            //    .AddGoogle(options =>
-            //    {
-            //        // register your IdentityServer with Google at https://console.developers.google.com
-            //        // enable the Google+ API
-            //        // set the redirect URI to http://localhost:5000/signin-google
-            //        options.ClientId = "copy client ID from Google here";
-            //        options.ClientSecret = "copy client secret from Google here";
-            //    });
+			// not recommended for production - you need to store your key material somewhere secure
+			builder.AddDeveloperSigningCredential();
         }
 
         public void Configure(IApplicationBuilder app)
